5f769cc4cd5b27be70e339b20514002aeae0963b
Sabli Jr <sablijr@yahoo.com>
2023-10-27 07:07:18 +0200

    Adding the favicon icon

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import "../Components/Register/Register.css";
 
 import Header from "../Components/TheHeader/index";
 import Login from "../Components/Register/Login";
 
 const Pricing = () => {
-  const date = new Date().getFullYear();
-
   return (
     <div>
       <Header />
       <Login />
-
-      <div className='copyDiv'>
-        <p>Wisties &copy;{date}</p>
-      </div>
     </div>
   );
 };
--- client/src/Pages/Signup.tsx
+++ client/src/Pages/Signup.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import "../Components/Register/Register.css";
 
 import Header from "../Components/TheHeader/index";
 import SignUp from "../Components/Register/SignUp";
 
 const Pricing = () => {
-  const date = new Date().getFullYear();
-
   return (
     <div>
       <Header />
       <SignUp />
-
-      <div className='copyDiv'>
-        <p>Wisties &copy;{date}</p>
-      </div>
     </div>
   );
 };
