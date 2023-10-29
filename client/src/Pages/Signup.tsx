1eb1ed6ac2d676fdccad11dfa0280ef2924589c1
Sabli Jr <sablijr@yahoo.com>
2023-10-29 21:33:21 +0100

    Fixing the landing page after deploying

--- client/src/Pages/Signup.tsx
+++ client/src/Pages/Signup.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "../Components/TheHeader/index";
 import SignUp from "../Components/Register/SignUp";
+import TheFooter from "../Components/Footer/index";
 
 const Pricing = () => {
   return (
     <div>
       <Header />
       <SignUp />
+      <TheFooter />
     </div>
   );
 };
