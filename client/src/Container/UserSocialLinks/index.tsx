afcefa71343abae1f8de52fcb7f0fa5282260ff0
Sabli Jr <sablijr@yahoo.com>
2023-11-20 11:36:02 +0100

    The functionality of adding social media links

--- client/src/Container/UserSocialLinks/index.tsx
+++ client/src/Container/UserSocialLinks/index.tsx
@@ -16,39 +16,30 @@ import Fansly from "../../Assets/UserIcons/Fansly.png";
 import Reddit from "../../Assets/UserIcons/Reddit.png";
 import Discord from "../../Assets/UserIcons/Discord.png";
 
+//React Icons
 import { CgClose } from "react-icons/cg";
 import { FiEdit } from "react-icons/fi";
 import { MdDelete } from "react-icons/md";
+import { FaPlus } from "react-icons/fa";
+import { RiCloseFill } from "react-icons/ri";
 
 type SocialMediaLinkFormProps = {
   socialLinksModule: Boolean;
   handleSocialLinksModule: () => void;
 };
 
-type socialLinksType = {
-  platform: string;
-  platformLink: string;
-}[];
-
-
 const SocialMediaLinkForm = ({
   socialLinksModule,
   handleSocialLinksModule,
 }: SocialMediaLinkFormProps) => {
+  const [linksModule, setLinksModule] = useState(false);
+  const [fillingSocialInfo, setFillingSocialInfo] =
+    useState<iCreatorSocialLinks>({
+      link: "",
+      icon: "",
+      platformName: "",
+    });
   const { creatorSocialLinks } = useUserInfoCOntext();
-
-  // const [selectedPlatform, setSelectedPlatform] = useState("");
-  // const [platform, setPlatform] = useState("");
-  // const [link, setLink] = useState("");
-  // const [fillingSocialInfo, setfillingSocialInfo] = useState<
-  //   socialLinksType | React.SetStateAction<socialLinksType>
-  // >([
-  //   {
-  //     platform: "",
-  //     platformLink: "",
-  //   },
-  // ]); 
-
 
   const socialMediaOptions = [
     { icon: Insta, platform: "Instagram" },
@@ -63,12 +54,6 @@ const SocialMediaLinkForm = ({
     { icon: Discord, platform: "Discord" },
   ];
 
-  // const handlePlatformChange = (
-  //   event: React.ChangeEvent<HTMLSelectElement>
-  // ) => {
-  //   setSelectedPlatform(event?.target.value);
-  // };
-
   const handleLinkChange = (
     event:
       | React.ChangeEvent<HTMLInputElement>
@@ -76,47 +61,43 @@ const SocialMediaLinkForm = ({
     field: string
   ) => {
     const value = event?.target?.value;
-    // setSocialLinks({ ...socialLinks, [field]: value });
+
+    if (field === "platform") {
+      const selectedPlatform = socialMediaOptions.find(
+        (option) => option.platform === value
+      );
+
+      if (selectedPlatform) {
+        setFillingSocialInfo({
+          ...fillingSocialInfo,
+          [field]: value,
+          icon: selectedPlatform.icon,
+        });
+      }
+    } else {
+      setFillingSocialInfo({ ...fillingSocialInfo, [field]: value });
+    }
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // Handle the submission of social media link (e.g., send to server)
-    // console.log(`Selected Platform: ${selectedPlatform}, Link: ${link}`);
+    // You can handle submission logic here if needed
   };
 
+  const handleAddLinks = () => {
+    setLinksModule(!linksModule);
+    // setCreatorSocialLinks((prevLinks) => [...prevLinks, fillingSocialInfo]);
+  };
 
   return (
-    <section className='AddingLinksModule'>
-      <CgClose className='closeLinksModule' onClick={handleSocialLinksModule} />
-      <h3>Add/Update Your Social Links</h3>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <select onChange={(e) => handleLinkChange(e, "platform")}>
-            <option value=''>Select Platform</option>
-            {socialMediaOptions.map((option) => (
-              <option key={option.platform} value={option.platform}>
-                {" "}
-                {/* value={selectedPlatform} */}
-                <img src={option.icon} alt='' />
-                {option.platform}
-              </option>
-            ))}
-            <option value='custom'>Custom</option>
-          </select>
-          <div>
-            <label>
-              Enter Link:
-              <input
-                type='text'
-                // value={socialLinks}
-                onChange={(e) => handleLinkChange(e, "platformLinks")}
-              />
-            </label>
-          </div>
-        </label>
-
-        {creatorSocialLinks ? (
+    <form onSubmit={handleSubmit}>
+      <section className='AddingLinksModule'>
+        <CgClose
+          className='closeLinksModule'
+          onClick={handleSocialLinksModule}
+        />
+        <h3>Add/Update Your Social Links</h3>
+        {(creatorSocialLinks as iCreatorSocialLinks[])?.length > 0 ? (
           creatorSocialLinks?.map((link: iCreatorSocialLinks) => {
             return (
               <div key={link.platformName}>
@@ -138,14 +119,55 @@ const SocialMediaLinkForm = ({
         ) : (
           <div>
             <p>You have not added any social media links yet.</p>
-            <button>Add Links</button>
           </div>
         )}
+
+        {linksModule ? (
+          <label className='linksLabel'>
+            <RiCloseFill
+              className='closeLinksModule'
+              style={{
+                top: ".42rem",
+                fontSize: "1rem",
+              }}
+              onClick={() => setLinksModule(!linksModule)}
+            />
+
+            <select
+              onChange={(e) => handleLinkChange(e, "platform")}
+              className='platformsSelection'>
+              <option value=''>Select Platform</option>
+              {socialMediaOptions.map((option) => (
+                <option key={option.platform} value={option.platform}>
+                  {option.platform}
+                </option>
+              ))}
+              <option value='custom'>Other</option>
+            </select>
+            <div>
+              <p>Enter Link:</p>
+              <input
+                type='text'
+                className='linkInput'
+                onChange={(e) => handleLinkChange(e, "platformLinks")}
+              />
+            </div>
+
+            <button id='addModuleId' onClick={handleAddLinks}>
+              Add
+            </button>
+          </label>
+        ) : null}
+        <button
+          className='addLinksBtn'
+          onClick={() => setLinksModule(!linksModule)}>
+          Add Links <FaPlus className='linksPlusIcon' />
+        </button>
         <button type='submit' className='saveLinks'>
           Save Links
         </button>
-      </form>
-    </section>
+      </section>
+    </form>
   );
 };
 
