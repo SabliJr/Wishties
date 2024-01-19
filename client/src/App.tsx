895aa4d891560aebcc807553d8527fa4f9acc8a7
Sabli Jr <sablijr@yahoo.com>
2024-01-19 12:10:09 +0100

    Adding google analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,13 @@ import LaRoutes from "./Routes";
 import { GlobalValuesProvider } from "./Context/globalValuesContextProvider";
 import { AuthProvider } from "./Context/authCntextProvider";
 
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-BVRQT2SHLP");
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
+
 const App = () => {
   return (
     <div className='App'>
